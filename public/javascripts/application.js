1745b37d252f3bcd8eae09c21bc7a50d3b9a213d
Tony Eichelberger <tony.eich@gmail.com>
2010-02-13 16:34:05 -0600

    added toggle

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function success() {
+  $('submit_setup_completed').toggle();
+  $('submit_audit_completed').toggle();
+  $('submit_setup').toggle();
+  $('submit_audit').toggle();
+}
+
+function failure() {
+  alert("failure");
+}
