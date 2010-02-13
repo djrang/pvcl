ed82db96cd453e4797a242afc681c620675fd6c5
Tony Eichelberger <tony.eich@gmail.com>
2010-02-13 17:01:34 -0600

    almost there

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,21 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function success() {
+function success_submit_setup() {
   $('submit_setup_completed').toggle();
   $('submit_audit_completed').toggle();
   $('submit_setup').toggle();
   $('submit_audit').toggle();
 }
 
+function success_submit_audit() {
+  $('submit_setup_completed').show();
+  $('submit_audit_completed').show();
+  $('submit_setup').hide();
+  $('submit_audit').hide();
+}
+
 function failure() {
   alert("failure");
 }
+
